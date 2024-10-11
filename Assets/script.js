cfa712e032f5c7e96ca11211fbe75ce6f8f1195b
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2024-10-10 21:33:09 -0500

    made changes to the add new book button's code

--- Assets/script.js
+++ Assets/script.js
@@ -4,26 +4,32 @@ let pageCount = 0;
 let pageGoal = 0;
 let readPages = 0;
 let readingNotes = [];
-let book = {};
+let book = [];
 let bookShelf = [];
 
+// Code for the "add new book" button
 const addNewBook = document.getElementById('add-book-button');
 
 addNewBook.addEventListener("click", function(){
-  let bookTitle = prompt("Please enter book title:");
-  let bookAuthor = prompt("Please enter the book's author:");
-  let pageCount = prompt("Please enter the number of pages:");
+  let j = 0;
+  bookName = prompt("Please enter book title:");
+  bookAuthor = prompt("Please enter the book's author:");
+  pageCount = prompt("Please enter the number of pages:");
 
-  if (!(pageCount > 0)){
-    return;
+   if (!(pageCount > 0)){
+     return;
   }
 
-  book = {
-    title: bookTitle,
-    author: bookAuthor,
-    numPages: Number(pageCount)
-  };
-  
+  pageGoal = prompt("Please enter the number of pages you want to set as your daily goal:");
+  readPages = prompt("Please enter the number of pages you've read today:");
+  const numNotes = prompt("Enter the number of notes you want to add");
+
+  for (i = 0; i < numNotes; i++){
+    readingNotes[i] = prompt("Enter your note:");
+  }
+
+  book = [bookName, bookAuthor, pageCount, pageGoal, readPages, readingNotes];
+
   bookShelf.push(book);
-  console.log(bookShelf);
-})
+  return(bookShelf);
+});
