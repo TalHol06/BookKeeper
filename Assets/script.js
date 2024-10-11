f45dc3a49b5ebc594352d7d61d98af6077b7f5b0
Ta'Lee Hollins-Bailey <taleeh98454@gmail.com>
2024-10-10 20:06:29 -0500

    added add new book button

--- Assets/script.js
+++ Assets/script.js
@@ -4,5 +4,26 @@ let pageCount = 0;
 let pageGoal = 0;
 let readPages = 0;
 let readingNotes = [];
-let book = [];
+let book = {};
 let bookShelf = [];
+
+const addNewBook = document.getElementById('add-book-button');
+
+addNewBook.addEventListener("click", function(){
+  let bookTitle = prompt("Please enter book title:");
+  let bookAuthor = prompt("Please enter the book's author:");
+  let pageCount = prompt("Please enter the number of pages:");
+
+  if (!(pageCount > 0)){
+    return;
+  }
+
+  book = {
+    title: bookTitle,
+    author: bookAuthor,
+    numPages: Number(pageCount)
+  };
+  
+  bookShelf.push(book);
+  console.log(bookShelf);
+})
